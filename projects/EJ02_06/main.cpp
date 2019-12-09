8bfd6f8b85bf3a857a87c78e27d1159878477102
Aitor Velez <aitorvt95@gmail.com>
2019-12-09 23:34:33 +0100

    EJ02_06 done

---
+++ projects/EJ02_06/main.cpp
@@ -0,0 +1,166 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "engine/input.hpp"
+#include "engine/window.hpp"
+
+#include <iostream>
+#include <math.h> 
+
+void handleInput() {
+	std::vector<std::pair<int, int>> keys = Input::instance()->getKeys();
+	for (auto& key : keys) {
+		std::cout << key.first << " - " << key.second << std::endl;
+	}
+}
+
+bool checkShader(uint32_t shader) {
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+		std::cout << "Error Compiling Shader " << infoLog << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool checkProgram(uint32_t program) {
+	int success;
+	char infoLog[512];
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(program, 512, nullptr, infoLog);
+		std::cout << "Error Linking Program " << infoLog << std::endl;
+		return false;
+	}
+	return true;
+}
+
+uint32_t createProgram() {
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location=0) in vec3 aPos;\n"
+		"void main() {\n"
+		"    gl_Position = vec4(aPos, 1.0);\n"
+		"}\0";
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main() {\n"
+		"    FragColor = vec4(0.6, 0.6, 0.1, 1.0);\n"
+		"}\0";
+
+	const uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+	checkShader(vertexShader);
+
+	const uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+	checkShader(fragmentShader);
+
+	const uint32_t program = glCreateProgram();
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+	checkProgram(program);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return program;
+}
+
+uint32_t createVertexData(uint32_t* VBO, uint32_t* EBO) {
+	float vertices[7 * 3];
+
+	vertices[0] = 0.0f;
+	vertices[1] = 0.0f;
+	vertices[2] = 0.0f;
+
+	int i = 1;
+	float r = sqrt(pow(0.25, 2) + pow(0.5, 2)); // definimos radio
+	float angle = 1.0472; //60º
+	float currentAngle = angle / 2; //30º
+
+	while (i < sizeof(vertices) / (sizeof(float) * 3)) {
+		vertices[i * 3] = r * cos(currentAngle);
+		vertices[(i * 3) + 1] = r * sin(currentAngle);
+		vertices[(i * 3) + 2] = 0.0f;
+		i++;
+		currentAngle += angle;
+	}
+
+	uint32_t indices[] = {
+		2, 0, 1,
+		3, 0, 2,
+		4, 0, 3,
+		5, 0, 4,
+		6, 0, 5,
+		1, 0, 6,
+	};
+
+	uint32_t VAO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, VBO);
+	glGenBuffers(1, EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	return VAO;
+}
+
+
+void render(uint32_t VAO, uint32_t program) {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(program);
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 6 * 3, GL_UNSIGNED_INT, nullptr);
+}
+
+int main(int, char* []) {
+	Window* window = Window::instance();
+
+	glClearColor(0.0f, 0.3f, 0.6f, 1.0f);
+
+	uint32_t VBO, EBO;
+	const uint32_t VAO = createVertexData(&VBO, &EBO);
+	const uint32_t program = createProgram();
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	while (window->alive()) {
+		handleInput();
+		render(VAO, program);
+		window->frame();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	glDeleteProgram(program);
+
+	return 0;
+}
